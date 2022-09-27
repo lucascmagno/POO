7b1c7fe92c5ad8bc11d92b891dec1277883097e6
Lucas Chagas Magno <109705062+lucascmagno@users.noreply.github.com>
2022-09-27 15:23:22 -0300

    Aula 4 get, set and construct

---
+++ Aula04/src/aula04/Aula04.java
@@ -0,0 +1,17 @@
+package aula04;
+public class Aula04 {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC", "Amarelo", 0.4f);
+        Caneta c2 = new Caneta("Esferografica","Preta",1.5f);
+        /*c1.setModelo("BIC");
+        c1.setPonta(0.5f);
+        //c1.status();
+        System.out.println("Tenho uma caneta "+c1.getModelo()+" de ponta "+c1.getPonta());
+        */
+        c1.status();
+        System.out.println("\n");
+        c2.status();
+    }
+    
+}
